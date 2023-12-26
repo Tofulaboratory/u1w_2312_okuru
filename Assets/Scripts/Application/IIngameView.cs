021092873c02127c22d8e57787bde6a871cffe89
Tofulaboratory <tofulab1373@gmail.com>
2023-12-26 21:58:49 +0900

    choise bar(view)

--- Assets/Scripts/Application/IIngameView.cs
+++ Assets/Scripts/Application/IIngameView.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
@@ -5,5 +6,7 @@ using UnityEngine;
 
 public interface IIngameView
 {
+    public IObservable<float> OnDecideParameter();
+    public void StopChoiseBarV();
     public void SetActive(bool isActivate);
 }
--- Assets/Scripts/Presentation/IngameView.cs
+++ Assets/Scripts/Presentation/IngameView.cs
@@ -1,12 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using TMPro;
+using UniRx;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class IngameView : ViewBase, IIngameView
 {
+    private Subject<float> _onDecideParameter = new();
+    public IObservable<float> OnDecideParameter() => _onDecideParameter;
+
+    public void StopChoiseBarV()
+    {
+        
+    }
+
     public void SetActive(bool isActivate)
     {
         base.SetActiveCanvasGroup(isActivate);
