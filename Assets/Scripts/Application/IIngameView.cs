2a17519c3cb9fcf92460a060c7a3395f07dc7aa3
おとうふラボ <tofulab1373@gmail.com>
2024-01-02 17:17:15 +0900

    パラメータ洗濯用barのviewの仮実装

--- Assets/Scripts/Application/IIngameView.cs
+++ Assets/Scripts/Application/IIngameView.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 public interface IIngameView
 {
     public IObservable<float> OnDecideParameter();
-    public void StopChoiseBarV();
+    public UniTask StopChoiseBar(BarType type, CancellationToken ct);
+    public void StartChoiseBar(BarType type);
     public void SetActive(bool isActivate);
 }
--- Assets/Scripts/Presentation/IngameView.cs
+++ Assets/Scripts/Presentation/IngameView.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using TMPro;
 using UniRx;
@@ -9,12 +10,58 @@ using UnityEngine.UI;
 
 public class IngameView : ViewBase, IIngameView
 {
+    [SerializeField] private Scrollbar choiseBarV;
+    [SerializeField] private Scrollbar choiseBarH;
+
     private Subject<float> _onDecideParameter = new();
     public IObservable<float> OnDecideParameter() => _onDecideParameter;
 
-    public void StopChoiseBarV()
+    private CancellationTokenSource _moveChoiseBarCts = new();
+
+    public async UniTask StopChoiseBar(BarType type, CancellationToken ct)
     {
-        
+        _moveChoiseBarCts?.Cancel();
+
+        //TODO
+        await UniTask.Delay(100000, cancellationToken: ct);
+
+        SetActiveChoiseBar(type, false);
+    }
+
+    public void StartChoiseBar(BarType type)
+    {
+        SetActiveChoiseBar(type, true);
+
+        if (!_moveChoiseBarCts.IsCancellationRequested) return;
+        _moveChoiseBarCts = new CancellationTokenSource();
+
+        MoveChoiseBar(type, _moveChoiseBarCts.Token).Forget();
+    }
+
+    private void SetActiveChoiseBar(BarType type, bool isActivate)
+    {
+        var bar = GetChoiseBar(type);
+        if (bar == null) return;
+        bar.gameObject.SetActive(isActivate);
+    }
+
+    private async UniTask MoveChoiseBar(BarType type, CancellationToken ct)
+    {
+        var bar = GetChoiseBar(type);
+        if (bar == null) return;
+
+        //TODO
+        await UniTask.Delay(100000, cancellationToken: ct);
+    }
+
+    private Scrollbar GetChoiseBar(BarType type)
+    {
+        switch (type)
+        {
+            case BarType.Vertical: return choiseBarV;
+            case BarType.Horizontal: return choiseBarH;
+            default: return null;
+        }
     }
 
     public void SetActive(bool isActivate)
